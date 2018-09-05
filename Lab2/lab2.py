75677bfcf2590501d5af6ba83fe3d94c3ed57c9b
YuntianLan <yl653@cornell.edu>
2018-09-05 14:52:55 -0400

    updated lab2 midway

---
+++ Lab2/lab2.py
@@ -0,0 +1,47 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+T = np.arange(0, 1.05, 0.05)
+V = np.array([0.041, 0.128, 0.449, 0.748, 0.785, 0.831, 0.774, 0.643, 0.606,\
+             0.187, 0.067, -0.332, -0.604, -0.688, -0.840, -0.818, -0.834,\
+            -0.640, -0.567, -0.179, -0.080])
+
+f2 = np.polyfit(T, V, 2, full = True)
+f3 = np.polyfit(T, V, 3, full = True)
+f4 = np.polyfit(T, V, 4, full = True)
+f5 = np.polyfit(T, V, 5, full = True)
+
+residual2 = f2[1][0]
+residual3 = f3[1][0]
+residual4 = f4[1][0]
+residual5 = f5[1][0]
+
+rms2 = residual2 / len(T)
+rms3 = residual3 / len(T)
+rms4 = residual4 / len(T)
+rms5 = residual5 / len(T)
+
+coe = f5[0]
+c5, c4, c3, c2, c1, c0 = coe
+func = lambda x: \
+c5 * x**5 + c4 * x**4 + c3 * x**3 + c2 * x**2 + c1 * x + c0
+V5 = list(map(func, list(T)))
+
+plt.xticks(np.arange(0, 1.2, step = 0.2))
+plt.yticks(np.arange(-1, 1.5, step = 0.5))
+axes = plt.gca()
+axes.set_ylim([-1,1])
+plt.xlabel('Time')
+plt.ylabel('Voltage')
+plt.title('Polynomial Curve Fitting')
+
+plt.plot(T, V, marker = 'o', linewidth = 0)
+plt.plot(T, V5)
+
+plt.show()
+
+
+# Solve for A
+A = 0
+for i in range(1, len(V)):
+    A += V[i] / np.sin(2 * np.pi * T[i])
