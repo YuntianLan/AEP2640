2b1608c04c164b31946f02e19d56fe157a8d6d03
YuntianLan <yl653@cornell.edu>
2018-09-12 16:35:49 -0400

    fuck this class

--- Lab3/plot.py
+++ Lab3/plot.py
@@ -9,9 +9,15 @@ with open('curve.txt') as f:
 
 lst = s.split('\t')
 lst[-1] = lst[-1][:-1]
+lst = lst[6:-1]
 
 volts = list(map(float, lst))
-time = np.arange(0, 2e-6 * len(volts), 2e-6)
+time = np.arange(0, xincr * len(volts), xincr)
 
 plt.plot(time, volts)
+
+plt.xlabel('Time')
+plt.ylabel('Voltage')
+plt.title('Oscilloscope Reading')
+
 plt.show()
