3f8a734083f12e7350ec2e3caeaf2ca4a0ba495c
YuntianLan <yl653@cornell.edu>
2018-09-19 16:19:38 -0400

    what the fuck

---
+++ Lab4/plot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xincr = 2e-5
+
+with open('curve2.txt') as f:
+    for line in f:
+        s = line
+
+lst = s.split('\t')
+lst[-1] = lst[-1][:-1]
+lst = lst[6:-1]
+
+volts = list(map(float, lst))
+volts = list(map(lambda x: x if x < 20 else x - 20.4, volts))
+time = np.arange(0, xincr * len(volts), xincr)
+
+plt.plot(time, volts)
+
+plt.xlabel('Time')
+plt.ylabel('Voltage')
+plt.title('Oscilloscope Reading')
+
+plt.show()
