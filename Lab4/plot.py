01d98e07b33f6d4d3ca35d2267234ad481421100
Yuntian Lan <yl653@cornell.edu>
2018-10-03 13:13:35 -0400

    with graph

--- Lab4/plot.py
+++ Lab4/plot.py
@@ -3,13 +3,15 @@ import numpy as np
 
 xincr = 2e-5
 
-with open('curve2.txt') as f:
+with open('waveform_hardware.txt') as f:
     for line in f:
         s = line
 
 lst = s.split('\t')
 lst[-1] = lst[-1][:-1]
 lst = lst[6:-1]
+
+lst = lst[:500]
 
 volts = list(map(float, lst))
 volts = list(map(lambda x: x if x < 20 else x - 20.4, volts))
